ee29f2eab50f898a4e485b1fe5a93b164023b793
Nagarjuna Vatti <nagarjunavatti@gmail.com>
2024-03-01 18:40:34 +0530

    Created Carbine Weapon and Muzzle Flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,7 +8,10 @@ public class Weapon : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] Transform FPCamera;
+    [SerializeField] float damage = 10f;
     [SerializeField] float range = 100f;
+    [SerializeField] ParticleSystem muzzleflash;
+    [SerializeField] GameObject BulletImpact;
     void Start()
     {
         
@@ -23,17 +26,36 @@ public class Weapon : MonoBehaviour
             Debug.Log("Shoot triggered");
         }
     }
-
     private void Shoot()
     {
+        PlaMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlaMuzzleFlash()
+    {
+        muzzleflash.Play();
+    }
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit "+hit.transform.gameObject.name);
+            Debug.Log("I hit " + hit.transform.gameObject.name);
+            ImpactEffect(hit);
+            EnemyHealth EHealth = hit.transform.GetComponent<EnemyHealth>();
+            if (EHealth == null) return;
+            EHealth.TakeDamage(damage);
         }
         else
         {
             return;
         }
+    }
+
+    private void ImpactEffect(RaycastHit hit)
+    {
+        GameObject Impact = Instantiate(BulletImpact, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(Impact, 0.1f);
     }
 }
